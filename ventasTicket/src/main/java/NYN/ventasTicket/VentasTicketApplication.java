cf486bd7bcc8d77316caf839d8a33f30f415b476
Cesar Carhuas <cesarcarhuas5@gmail.com>
2024-06-18 23:00:53 -0500

    CESAR xd
    agregando del spring

---
+++ ventasTicket/src/main/java/NYN/ventasTicket/VentasTicketApplication.java
@@ -0,0 +1,13 @@
+package NYN.ventasTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasTicketApplication.class, args);
+	}
+
+}
---
+++ ventasTicket/src/test/java/NYN/ventasTicket/VentasTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package NYN.ventasTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
